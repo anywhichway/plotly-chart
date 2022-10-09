161126ef1754a5af87bd7bea95f2ae4d93711326
syblackwell <syblackwell@anywhichway.com>
2022-10-09 08:00:56 -0700

    Initial public commit

---
+++ index.js
@@ -0,0 +1,33 @@
+self.properties({
+    connected() {
+        this.shadowRoot.appendChild(document.createElement("div"));
+    },
+    render() {
+        let json;
+        try {
+            json = JSON5.parse(this.innerHTML)
+        } catch(e) {
+            console.error(e);
+            json = {data:[],layout:{},config:{}};
+        }
+        const {data,layout,config} = json;
+        Plotly.newPlot(this.shadowRoot.firstElementChild,data,layout,config);
+    }
+})
+
+/*
+
+{ data: [
+{
+    x:['2020-10-04', '2021-11-04', '2023-12-04'],
+    y: [90, 40, 60],
+    type: 'scatter'
+}
+],
+layout:  {
+    title: 'Scroll and Zoom',
+    showlegend: false
+},
+config: {scrollZoom: true}
+}
+ */
